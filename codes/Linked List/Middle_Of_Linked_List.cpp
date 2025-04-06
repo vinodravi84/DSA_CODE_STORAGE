9f4fe0913cf5a577decc064b96686321bfe1bb3a
Vinod ravi <114280598+vinodravi84@users.noreply.github.com>
2025-04-06 18:54:33 +0530

    Add files via upload

---
+++ codes/Linked List/Middle_Of_Linked_List.cpp
@@ -0,0 +1,13 @@
+Node* findMiddle(Node* head) {
+    if (head == nullptr) return nullptr;
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
